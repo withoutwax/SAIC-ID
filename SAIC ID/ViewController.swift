d422ce5161466b0df51281617b4803b5ae477d01
Will Kim <rlagmlckd@gmail.com>
2018-10-16 18:24:02 -0500

    Initial Commit

---
+++ SAIC ID/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SAIC ID
+//
+//  Created by Will Kim on 10/16/18.
+//  Copyright © 2018 Will Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
